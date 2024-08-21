21fbdfea2f9287b5e3da57814451c3980e8d2699
Milan Sridhar <53534531+milansri09@users.noreply.github.com>
2024-08-21 14:04:54 -0400

    Add files via upload

---
+++ SnakeGame.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+//#include <graphics.h>
+#include <allegro5/allegro5.h>
+
+int main(void) {
+    //printf("Testing, testing");
+
+    // initwindow(600,600,"Snake Game");
+    // circle(0,0,30);
+    
+    // getch();
+    // closegraph();
+
+    al_init();
+    al_install_keyboard();
+    
+    
+    return 0;
+}
